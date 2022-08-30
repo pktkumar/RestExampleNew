ca6c2ee3478c51aaab858ae8acfd1a596df33d13
pktkumar <39775618+pktkumar@users.noreply.github.com>
2022-08-30 15:23:39 -0500

    cron job added
    cron job added

--- src/main/java/com/excample/controller/MyController.java
+++ src/main/java/com/excample/controller/MyController.java
@@ -2,25 +2,40 @@ package com.excample.controller;
 
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 
 @RestController
 public class MyController {
 
-        @RequestMapping("/")
-        public String index(){
-            return "my first page";
-        }
 
-        @RequestMapping("/Test")
-        public String indexNew(){
-            return "my second page";
-        }
-
-        @GetMapping(value = "/What")
-        public String noIdea(@RequestParam(value = "name") String myName){
-            System.out.println("myName" + myName);
-            return "no idea "+myName;
+    @RequestMapping("/Test")
+    public String indexNew() {
+        return "my second page";
     }
+
+
+//http://localhost:8080/What?name=Kumar
+
+    @GetMapping(value = "/What")
+    public String noIdea(@RequestParam(value = "name") String myName) {
+        System.out.println("myName" + myName);
+        return "no idea " + myName;
+
+    }
+
+    /*
+     * cron jobs for every 10 secs
+     * @EnableScheduling on @springbootapplication
+     * 
+     */
+    @Scheduled(cron = "*/10 * * * * *")
+    public void myCron() {
+        System.out.println("Method executed at every 5 seconds. Current time is :: " + new Date());
+    }
+
+
 }
