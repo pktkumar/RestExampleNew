d20c8295302c5abb3f4d6e78a077b6d251524b35
pktkumar <39775618+pktkumar@users.noreply.github.com>
2022-08-30 18:53:54 -0500

    updated with another API Call
    updated with another API Call

--- src/main/java/com/excample/controller/MyController.java
+++ src/main/java/com/excample/controller/MyController.java
@@ -1,9 +1,11 @@
 package com.excample.controller;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
 
@@ -11,14 +13,14 @@ import java.util.Date;
 @RestController
 public class MyController {
 
-
-    @RequestMapping("/Test")
-    public String indexNew() {
+//http://localhost:8090/Test
+/*    @RequestMapping("/Test")
+       public String indexNew() {
         return "my second page";
-    }
+    }*/
 
 
-//http://localhost:8080/What?name=Kumar
+//http://localhost:8090/What?name=Kumar
 
     @GetMapping(value = "/What")
     public String noIdea(@RequestParam(value = "name") String myName) {
@@ -30,12 +32,19 @@ public class MyController {
     /*
      * cron jobs for every 10 secs
      * @EnableScheduling on @springbootapplication
-     * 
+     *
      */
     @Scheduled(cron = "*/10 * * * * *")
     public void myCron() {
-        System.out.println("Method executed at every 5 seconds. Current time is :: " + new Date());
+        String url = "http://localhost:8080/Test";
+        RestTemplate restTemplate = new RestTemplate();
+        String str = restTemplate.getForObject(url, String.class);
+
+        System.out.println(str);
+
     }
+
+
 
 
 }
